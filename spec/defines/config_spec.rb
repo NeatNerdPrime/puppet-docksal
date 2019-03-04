a602f08d5113e9e1a744e52fd890ab3903f145a9
Marcus <marcus@microserve.io>
2019-03-04 12:07:08 +0000

    Added further unit tests

--- spec/defines/config_spec.rb
+++ spec/defines/config_spec.rb
@@ -15,9 +15,58 @@ describe 'docksal::config' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile }
-      it do
-        is_expected.to contain_file('/home/username/.docksal').with('ensure' => 'directory')
+      context "docksal class without any parameters" do
+        it { is_expected.to compile }
+        it do
+          is_expected.to contain_file('/home/username/.docksal').with('ensure' => 'directory')
+        end
+
+        it do
+          is_expected.to contain_file('/home/username/.docksal/docksal.env').with({
+            :ensure => 'file',
+            :content => /^$/,
+          }).that_requires('File[/home/username/.docksal]')
+        end
+      end
+
+      context "docksal class with ci set" do
+        let(:params) { super().merge(:ci => true) }
+        it do
+          is_expected.to contain_file('/home/username/.docksal/docksal.env').with({
+            :ensure => 'file',
+            :content => /CI=1\n/,
+          }).that_requires('File[/home/username/.docksal]')
+        end
+      end
+
+      context "docksal class with native_docker set" do
+        let(:params) { super().merge(:native_docker => true) }
+        it do
+          is_expected.to contain_file('/home/username/.docksal/docksal.env').with({
+            :ensure => 'file',
+            :content => /DOCKER_NATIVE=1\n/,
+          }).that_requires('File[/home/username/.docksal]')
+        end
+      end
+
+      context "docksal class with katacoda set" do
+        let(:params) { super().merge(:katacoda => true) }
+        it do
+          is_expected.to contain_file('/home/username/.docksal/docksal.env').with({
+            :ensure => 'file',
+            :content => /KATACODA=1\n/,
+          }).that_requires('File[/home/username/.docksal]')
+        end
+      end
+
+      context "docksal class with stats_optout set" do
+        let(:params) { super().merge(:stats_optout => true) }
+        it do
+          is_expected.to contain_file('/home/username/.docksal/docksal.env').with({
+            :ensure => 'file',
+            :content => /DOCKSAL_STATS_OPTOUT=1\n/,
+          }).that_requires('File[/home/username/.docksal]')
+        end
       end
     end
   end
